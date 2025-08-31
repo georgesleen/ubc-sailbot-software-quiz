10b4f4e4d78e01b3602b00c30eada9ee98289cf0
George Sleen <147893275+georgeSleen@users.noreply.github.com>
2025-08-31 00:59:42 -0700

    Write unit tests for bound_to_180()

--- python/test_standard_calc.py
+++ python/test_standard_calc.py
@@ -7,6 +7,24 @@ from standard_calc import bound_to_180, is_angle_between
 def test_bound_basic1():
     assert bound_to_180(0) == 0
 
+def test_bound_basic2():
+    assert bound_to_180(135) == 135
+
+def test_bound_intervals1():
+    assert bound_to_180(-180) == -180
+    assert bound_to_180(180) == -180
+
+def test_bound_out_one_revolution():
+    assert bound_to_180(200) == -160
+    assert bound_to_180(-200) == 160
+
+def test_bound_out_multiple_revolutions():
+    assert bound_to_180(760) == 40
+    assert bound_to_180(-1100) == -40
+
+def test_bound_intervals2():
+    assert bound_to_180(-720 - 180) == -180
+    assert bound_to_180(360 + 180) == -180
 
 """ Tests for is_angle_between() """
 
